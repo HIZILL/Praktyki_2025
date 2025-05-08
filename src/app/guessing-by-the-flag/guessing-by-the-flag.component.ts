9f1a172231ebe1624969da7718f007573b2bb605
Jakub Nocoń <j.nocon@wasko.pl>
2025-05-08 19:16:18 +0200

    Dodanie czasu na odp + błędne trzy odp + możliwość ponownego zagrania

--- src/app/guessing-by-the-flag/guessing-by-the-flag.component.ts
+++ src/app/guessing-by-the-flag/guessing-by-the-flag.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
@@ -9,13 +9,17 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
   templateUrl: './guessing-by-the-flag.component.html',
   styleUrls: ['./guessing-by-the-flag.component.scss']
 })
-export class GuessingByTheFlagComponent {
+export class GuessingByTheFlagComponent implements OnDestroy {
   kraje: any[] = [];
   obecnyKraj: any = null;
   opcje: string[] = [];
   czySprawdzono = false;
   czyPoprawna = false;
   punkty = 0;
+  bledy = 0;
+  czas = 10;
+  timer: any = null;
+  graSkonczona = false;
 
   constructor(private http: HttpClient) {
     this.http.get<any[]>('https://restcountries.com/v3.1/all?fields=name,flags')
@@ -26,8 +30,16 @@ export class GuessingByTheFlagComponent {
   }
 
   nowaRunda() {
+    if (this.bledy >= 3) {
+      this.graSkonczona = true;
+      this.stopTimer();
+      return;
+    }
+
     this.czySprawdzono = false;
     this.czyPoprawna = false;
+    this.czas = 10;
+    this.startTimer();
 
     const losoweKraje = this.shuffle([...this.kraje]).slice(0, 3);
     this.obecnyKraj = losoweKraje[Math.floor(Math.random() * 3)];
@@ -35,12 +47,50 @@ export class GuessingByTheFlagComponent {
   }
 
   sprawdz(wybor: string) {
+    this.stopTimer();
     this.czySprawdzono = true;
     this.czyPoprawna = wybor === this.obecnyKraj.name.common;
-    if (this.czyPoprawna) this.punkty++;
+
+    if (this.czyPoprawna) {
+      this.punkty++;
+    } else {
+      this.bledy++;
+    }
+
+    if (this.bledy >= 3) {
+      this.graSkonczona = true;
+    }
+  }
+
+  startTimer() {
+    this.stopTimer();
+    this.timer = setInterval(() => {
+      this.czas--;
+      if (this.czas <= 0) {
+        this.sprawdz("Błędna odpowiedź - czas minął");
+      }
+    }, 1000);
+  }
+
+  stopTimer() {
+    if (this.timer) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
   }
 
   shuffle(array: any[]): any[] {
     return array.sort(() => Math.random() - 0.5);
+  }
+
+  ngOnDestroy() {
+    this.stopTimer();
+  }
+
+  resetujGre() {
+    this.punkty = 0;
+    this.bledy = 0;
+    this.graSkonczona = false;
+    this.nowaRunda();
   }
 }
