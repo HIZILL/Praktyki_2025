7499ff13861d3f38e672ab207af7458a11ca139b
Jakub Nocoń <j.nocon@wasko.pl>
2025-05-20 12:51:04 +0200

    Działająca gra bez dodatków

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -6,30 +6,40 @@ import {
   AfterViewInit,
   OnDestroy
 } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { Map, MapStyle, config } from '@maptiler/sdk';
 import '@maptiler/sdk/dist/maptiler-sdk.css';
-
-import { RouterModule, Router } from '@angular/router';
-import type { Feature } from 'geojson';
+import { Feature } from 'geojson';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-map',
+  imports: [CommonModule],
   templateUrl: './map.component.html',
-  imports: [RouterModule],
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor(private router: Router) {}
-
   map: Map | undefined;
+  kraje: any[] = [];
+  obecnyKraj: any = null;
+  punkty = 0;
+  bledy = 0;
+  graSkonczona = false;
+  maksBledy = 3;
 
-  @ViewChild('map', { static: false })
-  private mapContainer!: ElementRef<HTMLElement>;
+  @ViewChild('map', { static: false }) private mapContainer!: ElementRef<HTMLElement>;
+
+  constructor(private router: Router, private http: HttpClient) {}
 
   ngOnInit(): void {
     config.apiKey = 'KX7bujj98HnA2Kyjh033';
+
+    this.http.get<any[]>('https://restcountries.com/v3.1/all?fields=name,flags').subscribe(data => {
+      this.kraje = data;
+      this.nowaRunda();
+    });
   }
 
   ngAfterViewInit() {
@@ -67,32 +77,45 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
           if (features && features.length > 0) {
             const feature = features[0] as Feature;
             const props = feature.properties as { [key: string]: any };
-            const countryName = props['ADMIN'] || props['name'] || 'Nieznany kraj';
-            console.log('Kliknięto kraj:', countryName);
-          } else {
-            console.log('Nie kliknięto w żaden kraj.');
+            const countryClicked = props['ADMIN'] || props['name'];
+
+            this.sprawdz(countryClicked);
           }
         });
 
         setTimeout(() => this.map?.resize(), 300);
       });
-
-      this.map.on('moveend', () => {
-        const center = this.map?.getCenter();
-        const zoom = this.map?.getZoom();
-        console.log('Nowe centrum:', center);
-        console.log('Nowy zoom:', zoom);
-      });
-    } else {
-      console.log('Mapa się jeszcze nie wczytała');
     }
   }
 
-  ngOnDestroy() {
-    this.map?.remove();
+  nowaRunda() {
+    if (this.bledy >= this.maksBledy) {
+      this.graSkonczona = true;
+      return;
+    }
+
+    this.obecnyKraj = this.kraje[Math.floor(Math.random() * this.kraje.length)];
+  }
+
+  sprawdz(wybor: string) {
+    const prawidlowa = this.obecnyKraj.name.common;
+
+    if (wybor === prawidlowa) {
+      alert(`✅ Dobrze! To był ${prawidlowa}`);
+      this.punkty++;
+    } else {
+      alert(`❌ Źle! To był: ${prawidlowa}`);
+      this.bledy++;
+    }
+
+    this.nowaRunda();
   }
 
   przejdzDoMenu() {
     this.router.navigate(['/']);
+  }
+
+  ngOnDestroy() {
+    this.map?.remove();
   }
 }
